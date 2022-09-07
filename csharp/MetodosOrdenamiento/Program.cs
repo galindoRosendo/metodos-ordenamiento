7158a5baa3576b82af77e691f4b05dc0d2bc781a
Rosendo Galindo <rosendo_galindo@hotmail.com>
2022-09-07 10:59:01 -0500

    ✨ feat: Se agrega metodo quicksort dotnet
    Se agrega arreglo desordenado
    Se agrega metodo quicksort en solucion de .net

--- csharp/MetodosOrdenamiento/Program.cs
+++ csharp/MetodosOrdenamiento/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MetodosOrdenamiento
 {
     class Program
     {
+        public static List<int> unsortedArray = new List<int>() { 5, 8, 9, 4, 3, 2, 1, 10, 6, 7 };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("QuickSort");
+                Console.WriteLine(string.Join(",", QuickSort(unsortedArray)));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        public static List<int> QuickSort(List<int> unsortedArray)
+        {
+            List<int> sortArray = new List<int>();
+
+            if (unsortedArray.Count < 1)
+            {
+                return sortArray;
+            }
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            int pivot = unsortedArray[0];
+
+            for (int i = 1; i < unsortedArray.Count; i++)
+            {
+                if (unsortedArray[i] < pivot)
+                {
+                    left.Add(unsortedArray[i]);
+                }
+                else
+                {
+                    right.Add(unsortedArray[i]);
+                }
+            }
+
+            return sortArray.Concat(QuickSort(left)).Concat(new List<int>() { pivot }).Concat(QuickSort(right)).ToList();
         }
     }
 }
