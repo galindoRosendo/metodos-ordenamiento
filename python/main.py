76f0c96e37cf9be7376e3f7db8fd5d7e7977d859
Rosendo Galindo <rosendo_galindo@hotmail.com>
2022-09-14 14:36:30 -0500

    ✨ feat: Se agrega metodo radix python

--- python/main.py
+++ python/main.py
@@ -2,6 +2,7 @@ import sys
 import numpy as geek
 
 class Menu:
+    data = [121, 432, 564, 23, 1, 45, 788]
     def __init__(self):
         print('')
     def display_menu(self):
@@ -13,9 +14,9 @@ class Menu:
         choice = input()
      
         if choice == "1":
-            self.quickSort()
+            self.quickSort(self.data)
         elif choice == "2":
-            self.radixSort()
+            self.radixSort(self.data)
            
     def quickSort(array):
         
@@ -34,9 +35,43 @@ class Menu:
                 right.append(array[i])
             control+=1
         return geek.concatenate(self.quickSort(left), pivot, self.quickSort(right))
-    
-    def radixSort(arg):
-        print('[]')
+
+    # Main function to implement radix sort
+    def radixSort(array):
+        # Get maximum element
+        max_element = max(array)
+
+        # Apply counting sort to sort elements based on place value.
+        place = 1
+        while max_element // place > 0:
+            countingSort(array, place)
+            place *= 10
+        
+         # Using counting sort to sort the elements in the basis of significant places
+        def countingSort(array, place):
+            size = len(array)
+            output = [0] * size
+            count = [0] * 10
+
+            # Calculate count of elements
+            for i in range(0, size):
+                index = array[i] // place
+                count[index % 10] += 1
+
+            # Calculate cumulative count
+            for i in range(1, 10):
+                count[i] += count[i - 1]
+
+            # Place the elements in sorted order
+            i = size - 1
+            while i >= 0:
+                index = array[i] // place
+                output[count[index % 10] - 1] = array[i]
+                count[index % 10] -= 1
+                i -= 1
+
+            for i in range(0, size):
+                array[i] = output[i]
         
 if __name__ == "__main__":
     Menu().run()
